05abe5114713a04fa3fc0e9d8dc11d704ec72e05
FidelGastro <travixe@gmail.com>
2022-08-24 21:31:29 +0100

    coin pickup

--- Assets/script/McPiece.cs
+++ Assets/script/McPiece.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectionPieces : MonoBehaviour
+public class McPiece : MonoBehaviour
 {
     public GameObject porteO;
     public GameObject porteL;
@@ -18,18 +18,22 @@ public class CollectionPieces : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(PieceCount >= 3)
+        if (PieceCount >= 3)
         {
             porteO.SetActive(true);
             porteL.SetActive(false);
         }
     }
 
-     private void OnTriggerEnter2D(Collider2D collision){
-        PieceCount += 1;
-        //Destroy(gameObject);
-        piece.GetComponent<SpriteRenderer>().enabled = true;
-        piece.GetComponent<Collider2D>().enabled = true;
-        Debug.Log(PieceCount);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "coin")
+        {
+            PieceCount += 1;
+            Destroy(collision.gameObject);
+            collision.GetComponent<SpriteRenderer>().enabled = true;
+            collision.GetComponent<Collider2D>().enabled = true;
+            Debug.Log(PieceCount);
+        }
     }
 }
